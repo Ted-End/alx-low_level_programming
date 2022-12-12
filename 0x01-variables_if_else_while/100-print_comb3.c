6d2c6d563ddd46471232da6592ebe1e26e736ca0
Ted-End <tewodrosendrias91@gmail.com>
2022-12-12 14:30:29 -0800

    different non repeting numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,16 +9,23 @@ int main(void)
 	int tens = '0';
 
 	for (tens = '0'; ones <= '9'; tens++)/*prints ones digit*/
-	{		
+	{
 		for (ones = '0'; ones <= '9'; ones++) /*prints ones digit*/
-		{	
-			if(!((ones == tens) || (tens > ones))) /*eliminates repition*/
+		{
+			if (!((ones == tens) || (tens > ones))) /*eliminates repition*/
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(tens);
+				putchar(ones);
+				if (!(ones == '9' && tens == '8)) /*adds comma and space*/
+				{
+					putchar(',');
+					putchair(' ');
+
+				}
 			}
 		}
 	}
 	putchar('\n');
+
 	return (0);
 }
