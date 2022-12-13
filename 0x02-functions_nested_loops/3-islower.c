83c6adf66ecd5500adce8754a00e2aed30c5237f
Ted-End <tewodrosendrias91@gmail.com>
2022-12-13 15:38:09 -0800

    is alpha

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * _islower - check if a char is lowecase
+ * c: is the char to be checked
  * Return: 1 if char is lowecase, otherwise 0
- * Parameter: c
  */
 int _islower(int c)
 {
