cf1d8c2319a5cac05f74d3a07ce6d5ba6f2f0867
Ted-End <tewodrosendrias91@gmail.com>
2022-12-13 15:11:24 -0800

    lowerchase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
- * main - lower case
+ * print_alphabet - print all alphabet in lowercase
  * Return: Always 0
  */
-int main(void)
+int print_alphabet(void)
 {
 	char a;
+
 	for (a = 'a'; a <= 'z'; a++)
 	{
 		_putchar(a);
+		_putchar('\n');
 	}
-	return (0);
 }
