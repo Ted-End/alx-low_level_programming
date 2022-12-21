a89a6d4d0aae7c5e99c86c1ea98aff0c5bb785c6
Ted-End <tewodrosendrias91@gmail.com>
2022-12-20 16:26:36 -0800

    randomPass

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - entry point
  * Return: Always 0
  */
 int main(void)
 {
+	int pass, sum;
+
+	srand(time(NULL));
+	sum = 0;
+	while (sum <= 2645)
+	{
+		pass = (rand() % 128);
+		sum += pass;
+		prinf("%c", pass);
+	}
+	printf("%c", 2772 - sum);
 	return (0);
 }
