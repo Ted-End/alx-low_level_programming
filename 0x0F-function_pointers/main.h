310edced7a005046d44b39f12745cec43a22d4a1
Ted-End <tewodrosendrias91@gmail.com>
2023-01-20 01:29:57 -0800

    print name

--- 0x0F-function_pointers/main.h
+++ 0x0F-function_pointers/main.h
@@ -1,6 +1,10 @@
-#ifndef HEADER_H
-#define HEADER_H
+#ifndef MAIN_H
+#define MAIN_H
+#include <stddef.h>
 
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+int int_index(int *array, int size, int (*cmp)(int));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
